3241572201c8ac0c931c85b6b01f7dcd5fb6f7a5
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-04-10 03:17:46 +0300

    Queen checks done

--- ChessLogic/src/Pieces/Queen.cpp
+++ ChessLogic/src/Pieces/Queen.cpp
@@ -11,19 +11,53 @@ void Queen::move(string to, char board[][8]){
 }
 
 bool Queen::isValidMove(string to, char board[][8]) {
-	char file = currentSquare.at(0);
-	int rank = currentSquare.at(1) - 48;
-	
-	cout << file << " " << rank << endl;
-	
-	char toFile = to.at(0);
-	int toRank = to.at(1) - 48;
-	
-	cout << toFile << " " << toRank << endl;
-	
+	int* fromCoords = translateSquare(currentSquare);
 	int* toCoords = translateSquare(to);
-	
-	// checks
-	
-	return true;
+
+	int fromRow = fromCoords[0], fromCol = fromCoords[1], toRow = toCoords[0], toCol = toCoords[1];
+
+	int rowDiff = abs(fromRow - toRow);
+    int colDiff = abs(fromCol - toCol);
+
+    // Check if the move is valid for the bishop (diagonal movement)
+    if (rowDiff == colDiff) {
+        int rowStep = (toRow > fromRow) ? 1 : -1;
+        int colStep = (toCol > fromCol) ? 1 : -1;
+        int i = fromRow + rowStep;
+        int j = fromCol + colStep;
+        while (i != toRow && j != toCol) {
+            if (board[i][j] != ' ') {
+                return false;
+            }
+            i += rowStep;
+            j += colStep;
+        }
+        return true;
+    }
+
+    // Check if the move is valid for the rook (horizontal/vertical movement)
+    if (fromRow == toRow) {
+        int colStep = (toCol > fromCol) ? 1 : -1;
+        int j = fromCol + colStep;
+        while (j != toCol) {
+            if (board[fromRow][j] != ' ') {
+                return false;
+            }
+            j += colStep;
+        }
+        return true;
+    }
+    if (fromCol == toCol) {
+        int rowStep = (toRow > fromRow) ? 1 : -1;
+        int i = fromRow + rowStep;
+        while (i != toRow) {
+            if (board[i][fromCol] != ' ') {
+                return false;
+            }
+            i += rowStep;
+        }
+        return true;
+    }
+
+    return false;
 }
