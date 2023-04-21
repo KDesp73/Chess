1129ff233eb64c08cf83b7db3a139e5c1029a647
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-04-22 01:02:42 +0300

    board methods split into 2 files

---
+++ ChessLogic/src/Board/Board.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+
+#include "./board_declarations.h"
+
+void Board::printBoard() { Board::printBoard("white"); }
+
+void Board::printBoard(string playingAs) {
+    if (playingAs == "white") {
+        cout << "   ABCDEFGH" << endl << endl;
+        for (int i = 7; i >= 0; i--) {
+            cout << i + 1 << "  ";
+            for (int j = 0; j < sizeof(board[i]); j++) {
+                cout << board[i][j];
+            }
+            cout << "  " << i + 1 << endl;
+        }
+        cout << endl << "   ABCDEFGH" << endl << endl;
+    } else if (playingAs == "black") {
+        cout << "   HGFEDCBA" << endl << endl;
+        for (int i = 0; i <= 7; i++) {
+            cout << i + 1 << "  ";
+            for (int j = 0; j < sizeof(board[i]); j++) {
+                cout << board[i][j];
+            }
+            cout << "  " << i + 1 << endl;
+        }
+        cout << endl << "   HGFEDCBA" << endl << endl;
+    } else {
+        cout << "Incorrect argument" << endl;
+    }
+}
+
+void Board::printBigBoard() { Board::printBigBoard("white"); }
+
+void Board::printBigBoard(string playingAs) {
+    if (playingAs == "white") {
+        cout << "     A   B   C   D   E   F   G   H " << endl;
+        cout << "   +---+---+---+---+---+---+---+---+" << endl;
+        for (int i = 7; i >= 0; i--) {
+            cout << i + 1 << "  ";
+            for (int j = 0; j < sizeof(board[i]); j++) {
+                cout << "| " << board[i][j] << " ";
+            }
+            cout << "|  " << i + 1 << endl;
+            cout << "   +---+---+---+---+---+---+---+---+" << endl;
+        }
+        cout << "     A   B   C   D   E   F   G   H " << endl;
+    } else if (playingAs == "black") {
+        cout << "     H   G   F   E   D   C   B   A " << endl;
+        cout << "   +---+---+---+---+---+---+---+---+" << endl;
+        for (int i = 0; i <= 7; i++) {
+            cout << i + 1 << "  ";
+            for (int j = 0; j < sizeof(board[i]); j++) {
+                cout << "| " << board[i][j] << " ";
+            }
+            cout << "|  " << i + 1 << endl;
+            cout << "   +---+---+---+---+---+---+---+---+" << endl;
+        }
+        cout << "     H   G   F   E   D   C   B   A " << endl;
+
+    } else {
+        cout << "Incorrect argument" << endl;
+    }
+}
+
+void Board::scanBoard(vector<Piece*> whitePieces, vector<Piece*> blackPieces) {
+    string white_square, black_square;
+    int *white_coords, *black_coords;
+
+    for (int i = 0; i < 16; i++) {
+        white_square = whitePieces.at(i)->currentSquare;
+        black_square = blackPieces.at(i)->currentSquare;
+
+        white_coords = translateSquare(white_square);
+        black_coords = translateSquare(black_square);
+
+        board[*white_coords][*(white_coords + 1)] =
+            whitePieces.at(i)->boardChar;
+        board[*black_coords][*(black_coords + 1)] =
+            blackPieces.at(i)->boardChar;
+    }
+}
