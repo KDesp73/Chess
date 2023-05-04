8e98f173f0d98a57982f26eb9f4af2a387dc17b5
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-05-05 00:11:57 +0300

    move struct added for future use

--- ChessLogic/src/Board/board_declarations.h
+++ ChessLogic/src/Board/board_declarations.h
@@ -11,18 +11,18 @@ using namespace std;
 //board_functions.cpp
 
 
-int* translateSquare(string move);
+int* translateSquare(string square);
 string translateSquare(int* coords);
 
 bool makeMove(string from, string to, char board[][8]);
 
 char pieceToMove(int* coords, char board[][8]);
 
-char pieceToMove(string move, char board[][8]);
+char pieceToMove(string square, char board[][8]);
 
 void printCoords(int* coords);
 
-void printCoords(string move);
+void printCoords(string square);
 
 
 
@@ -34,4 +34,8 @@ vector<Piece*> squareIsAttacked(string square, Pieces *p, char board[][8]);
 
 bool isValidSquare(string square);
 
+struct Move{
+    string from;
+    string to;
+};
 
