7f3d839b69eaac6be3fa253d41273f140571ec19
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-06-14 03:24:49 +0300

    cleaning

--- Chess/src/Game/game_logic.h
+++ Chess/src/Game/game_logic.h
@@ -8,5 +8,11 @@
 using namespace std;
 
 namespace Game{
-    Board start(string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", string playing = "white", bool showMaterial = true, bool showMoves = true, string prompt_type = Board::SEPERATE);
+    Board start(
+        string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+        string playing = "white",
+        bool showMaterial = true,
+        bool showMoves = true,
+        string prompt_type = Board::SEPERATE
+    );
 };
